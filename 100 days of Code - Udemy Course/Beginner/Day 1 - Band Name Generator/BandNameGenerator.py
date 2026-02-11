7b9044184d27c36744e859a0f7f76b30f1acb104
Vinícius Ribeiro Peixoto <vrp250606@gmail.com>
2026-02-10 21:12:22 -0300

    add(main_structure): Defined the repo structure
    I have defined the repository structure. And to begin I upload the first project of the 100 Days of Code Course.

---
+++ 100 days of Code - Udemy Course/Beginner/Day 1 - Band Name Generator/BandNameGenerator.py
@@ -0,0 +1,9 @@
+print("Hello There!")
+name = input("What's your name? ")
+print("Hello " + name)
+print("Let's find out your band name!\n")
+hometown = input("What is your hometown? ")
+pet = input("\nWhat's your pet's name? Or how'd you call it"
+      + " if you had one? ")
+
+print("Your band name is " + hometown + " " + pet)
